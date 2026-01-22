ce4435b23068ece92a4538cf3b842543d5261ba6
Alexa-Web-com <aleksandra.wilczynska.plgdy@gmail.com>
2026-01-22 13:48:50 +0100

    fix: routing basename

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/nie-ma-problemu">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/otwieraj" element={<Otwieraj />} />
