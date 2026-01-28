6378e2e11307045532c916160dc1d16a33660222
Damian <damian.wilczynski.pl@gmail.com>
2026-01-28 12:35:10 +0100

    add vite.config plugins

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
+  plugins: [react()].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
