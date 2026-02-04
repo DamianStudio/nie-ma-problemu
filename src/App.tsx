b21020c06c21c571c42274a9b26a16e158b19c54
Alexa-Web-com <aleksandra.wilczynska.plgdy@gmail.com>
2026-02-04 10:37:46 +0100

    chore: opt-in to v7_startTransition to resolve react router warnings

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,12 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <HashRouter>
+      <HashRouter
+        future={{
+          v7_startTransition: true,
+          v7_relativeSplatPath: true,
+        }}
+      >
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/otwieraj" element={<Otwieraj />} />
